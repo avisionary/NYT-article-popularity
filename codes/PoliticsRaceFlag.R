74dec99f508cb2549c441e88e7ea479981aab528
Olivier Kuhn de Chizelle <oyk3@georgetown.edu>
2022-04-24 21:13:21 -0400

    changing politics/Flags Viz

--- codes/PoliticsRaceFlag.R
+++ codes/PoliticsRaceFlag.R
@@ -4,6 +4,7 @@
 # Libraries
 library(ggplot2)
 library(ggthemes)
+library(ggtext)
 
 # Load in default dataset
 data = read.csv('../data/nyt-articles-2020.csv')
@@ -38,28 +39,33 @@ counts = table(data$is_racial, data$is_political)
 counts = data.frame(counts)
 colnames(counts)[1] = 'is_racial'
 colnames(counts)[2] = 'is_political'
+counts$is_racial = ifelse(counts$is_racial == 1, 'yes','no')
+counts$is_political = ifelse(counts$is_political == 1, 'yes','no')
 
 ### Let's make a Bar Graph showing Racial and Political Presence
 Race_Politics_Barplot = ggplot(data = counts, aes(x = is_political, y = Freq, fill = is_racial)) +
   geom_bar(stat = 'identity', position = 'dodge') +
   xlab('is_political') +
   ylab('Frequency') +
-  scale_fill_manual(values=c("darkred",
-                             "black",
-                             "red",
-                             "darkblue")) +
-  ggtitle('Frequency of Political and Racial NYT articles') +
-  theme(text = element_text(family = 'mono'),
-        axis.text.y = element_text(size = 7, angle = 0),
+  theme_classic() +
+  scale_fill_manual(values = c("#fdc086","#beaed4")) +
+  labs(x = 'Discusses Politics',
+       y = 'Frequency',
+       title = paste0("**How many articles discuss *politics* or *race* ?**
+                      \n<span style='font-size:11pt'> 
+                      <span style='color:#fdc086;'>Does</span> or
+                      <span style='color:#beaed4;'>does not</span> discuss Race
+                      </span>"),
+       fill = 'Discusses Race') +
+  theme(plot.title = ggtext::element_markdown(lineheight = 1.1),
+        plot.caption = ggtext::element_markdown(lineheight = 1.1),
+        legend.text = ggtext::element_markdown(size = 11),
+        axis.text.y = element_text(size = 8, angle = 0),
         axis.text.x = element_text(size = 11),
         axis.title.y = element_text(size = 14),
-        axis.title.x = element_text(size = 12),
-        plot.title = element_text(size = 15),
-        plot.background = element_rect(fill = '#fff1d4'),
-        panel.background = element_rect(fill = '#fff1d4'),
-        panel.grid.major = element_line(colour ='#e3d5b8'),
-        panel.grid.minor = element_line(colour ='#e3d5b8'),
-        legend.background = element_rect(fill = '#fff1d4'))
+        axis.title.x = element_text(size = 12))
+
+plot(Race_Politics_Barplot)
 
 ggsave('../images/Race_Politics_BarPlot.png', plot = Race_Politics_Barplot)
 plot(Race_Politics_Barplot)
